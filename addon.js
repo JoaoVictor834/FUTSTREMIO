538a4b50a007fd0b180d2e223101ca613d8def97
JoaoVictor834 <jv0888854@gmail.com>
2025-07-15 17:31:24 -0400

    initial commit

---
+++ addon.js
@@ -0,0 +1,78 @@
+const { addonBuilder, serveHTTP, getRouter } = require('stremio-addon-sdk')
+const SOURCES = require('./sources.json')
+
+const builder = new addonBuilder({
+    id: 'org.joao.tvefutebolhttp',
+    version: '1.0.0',
+
+    name: 'TV/FUTEBOL BR',
+
+    resources: ['stream', 'catalog'],
+    types: ['tv'],
+    "catalogs": [
+        {
+            type: 'tv',
+            id: 'top'
+        }
+    ],
+    idPrefixes: ['tvid']
+})
+
+const met = []
+SOURCES.forEach(s => {
+
+    met.push({
+        id: `tvid${s.id}`,
+        name: s.name,
+        releaseInfo: '2025',
+        poster: s.poster,
+        posterShape: 'landscape',
+        type: 'tv',
+        description: "LIVE DO CANAL " + s.name.toUpperCase().toString()
+    })
+});
+
+
+
+builder.defineCatalogHandler(function (args) {
+    
+    
+    if (args.type === 'tv' && args.id === 'top') {
+
+
+
+
+        try {
+            return Promise.resolve({ metas: [...met] })
+        } catch (error) {
+            console.error(error)
+        }
+
+    } else {
+        return Promise.resolve({ metas: [] })
+
+    }
+})
+
+builder.defineStreamHandler(function (args) {
+
+   const s = SOURCES.find(s => `tvid${s.id}` === args.id)
+
+        if (args.type === 'tv') {
+
+                const stream = { name: s.name, description: `LIVE AO VIVO DO ${s.name.toUpperCase()}`, type: 'tv', url: `http://localhost:8080/stream/${s.name}.m3u8` }
+                return Promise.resolve({ streams: [stream] })
+            } else {
+                // otherwise return no streams
+                return Promise.resolve({ streams: [] })
+            }
+        
+    
+})
+
+//serveHTTP(builder.getInterface(), { port: 8080 })
+
+console.log("addon.js")
+router = getRouter(builder.getInterface())
+
+module.exports = router
---
+++ main.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const axios = require('axios');
+const router = require('./addon');
+
+const app = express();
+
+const SOURCES = require('./sources.json')
+const ALLOWED_HOSTS = new Set([
+  'embmaxtv.online',
+]);
+
+const HEADERS = {
+  Referer: 'https://embedcanaistv.com/',
+  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0',
+  Accept: '*/*',
+  'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
+  Connection: 'keep-alive',
+};
+
+app.get('/stream/:id.m3u8', async (req, res) => {
+  const playlistUrl = SOURCES.find(s => s.name === req.params.id).url
+  if(!playlistUrl) return console.error('Erro em playlisturl')
+
+  try {
+    const response = await axios.get(playlistUrl, { headers: HEADERS });
+    const playlist = response.data;
+
+    const baseUrl = playlistUrl.substring(0, playlistUrl.lastIndexOf('/') + 1);
+
+    // Reescreve para o segment no proxy
+    const newPlaylist = playlist.split('\n').map(line => {
+      if (line && !line.startsWith('#') && line.endsWith('.ts')) {
+        const segmentFullUrl = new URL(line, baseUrl).href;
+        const encodedSegmentUrl = encodeURIComponent(segmentFullUrl);
+        return `/segment?url=${encodedSegmentUrl}`;
+      }
+      return line;
+    }).join('\n');
+
+    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+    res.send(newPlaylist);
+  } catch (err) {
+    console.error('Erro ao buscar playlist:', err.message);
+    res.status(502).send('Erro ao buscar playlist');
+  }
+});
+
+// Segmentos m3u8
+app.get('/segment', async (req, res) => {
+  const segmentUrl = req.query.url;
+  if (!segmentUrl) {
+    return res.status(400).send('Faltando URL de Segmento');
+  }
+
+  try {
+    const decodedUrl = decodeURIComponent(segmentUrl);
+    const parsedHost = new URL(decodedUrl).hostname;
+
+    // Só permitimos proxy para hosts autorizados
+    if (!ALLOWED_HOSTS.has(parsedHost)) {
+      return res.status(403).send('Host não permitido');
+    }
+
+    const response = await axios.get(decodedUrl, {
+      headers: HEADERS,
+      responseType: 'stream',
+      timeout: 30000,
+    });
+
+    res.setHeader('Content-Type', 'video/MP2T');
+    response.data.pipe(res);
+  } catch (err) {
+    console.error('Erro no segmento:', err.message);
+    res.status(502).send('Erro ao buscar segmento');
+  }
+});
+
+app.use('/', router);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Proxy rodando na porta ${PORT}`);
+});
